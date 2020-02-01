7b240af9110d7fdd2ad90895bd09a3c0913194f1
Alan <sasukemaru.gamer@gmail.com>
2020-02-01 15:56:39 -0300

    adjust_new_mobile_style

--- Tes Consultoria Site/script.js
+++ Tes Consultoria Site/script.js
@@ -1,12 +1,32 @@
+var checkLoadBody;
 
-	var checkLoadBody;
+var onLoadBody = () => {
+  if (!checkLoadBody) {
+    document
+      .querySelectorAll(".boxSlider")[0]
+      .setAttribute("style", "--s-height: " + window.innerHeight);
+    checkLoadBody = true;
+  }
+};
 
-	var onLoadBody = () => {	
-		if(!checkLoadBody) { 
-			document.querySelectorAll(".boxSlider")[0].setAttribute("style", "--s-height: "+ window.innerHeight);
-			checkLoadBody = true;
-		}
-	};
+onLoadBody();
 
-	onLoadBody();
-	
+function openMobile() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+// NAV FLOW
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+  document.getElementById("whats_over").style.position = "inherit";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+  document.getElementById("whats_over").style.position = "fixed";
+}
